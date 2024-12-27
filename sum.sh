1562d4d8561b7714a895c4c3420b5eb69cf18c85
Durgarao-gunja365 <141761258+Durgarao-gunja365@users.noreply.github.com>
2024-12-27 12:27:41 +0530

    bashscript file

---
+++ sum.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Enter your first number : "
+read num1
+echo "Enter your second number"
+read num2
+sum=$((num1+num2))
+echo "Addition of two numbers is $sum "
+
+echo "Multiplication : "
+echo "Enter your first number : "
+
+read num1
+echo "Enter your second number"
+read num2
+mul=$((num1*num2))
+echo "Multiplication of two numbers is $mul "
